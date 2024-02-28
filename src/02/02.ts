b7bdf3af06f454f29e2b479a589969b20d1655de
fireonex <mari.krulikovskaya@mail.ru>
2024-02-28 22:34:25 +0300

    first commit

---
+++ src/02/02.ts
@@ -0,0 +1,64 @@
+
+type TechnologiesType = {
+    id: number
+    title: string
+}
+
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type StudentType = {
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechnologiesType>
+}
+
+
+
+export const student: StudentType = {
+    name: "Katara",
+    age: 18,
+    isActive: false,
+    address: {
+        streetTitle: "Baltian 2",
+        city: {
+            title: "Pskov",
+            countryTitle: "Russia"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "REACT"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "JS"
+        },
+        {
+            id: 4,
+            title: "NodeJS"
+        },
+        {
+            id: 5,
+            title: "HTML"
+        }
+    ]
+
+}
+
+console.log(student.address.city.countryTitle)
+console.log(student.technologies[0].title)
---
+++ src/02/02_02.ts
@@ -0,0 +1,10 @@
+export type HouseType = {
+
+}
+
+export type CityType = {
+    title: string
+    houses: Array<string>
+    governmentBuildings: Array<string>
+    citizensNumber: number
+}
